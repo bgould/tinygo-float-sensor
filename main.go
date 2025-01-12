32924581c86d3959eae727b7fb06a54ba29a8fae
BCG <bcgould1@gmail.com>
2025-01-11 23:57:18 -0500

    code refactoring

--- main.go
+++ main.go
@@ -1,53 +1,40 @@
+//go:build arduino_nano33
+
 package main
 
 import (
-	"errors"
 	"machine"
 	"time"
 
 	"tinygo.org/x/bluetooth"
+	"tinygo.org/x/drivers"
+	"tinygo.org/x/drivers/lsm6ds3"
 )
 
 const (
 	interval         = 100 * time.Millisecond
 	tempReadInterval = 5 * time.Second
 	localName        = "Sump Sensor"
-
-	binarySensorIdx1 = 2
-	binarySensorIdx2 = 4
-	tempSensorIdx    = 6
 )
 
 var (
-	adapter = bluetooth.DefaultAdapter
-
-	btHomeAdvInterval = bluetooth.NewDuration(interval)
-	btHomeServiceUUID = bluetooth.New16BitUUID(0xFCD2)
-	btHomeServiceData = []byte{0x40, 0x20, 0x01, 0x20, 0x01, 0x45, 0x00, 0x00}
-
-	errTemperatureNotSupported = errors.New("temperature not supported")
-	errTemperatureNotAvailable = errors.New("temperature not available")
-
-	floatSensor1 = NewFloatSensor(readPin1)
-	floatSensor2 = NewFloatSensor(readPin2)
+	tempSensor   = NewTemperatureSensor(machine.I2C0)
+	floatSensor1 = NewFloatSensor(machine.D3)
+	floatSensor2 = NewFloatSensor(machine.D5)
+	bthome       = NewBTHome(interval, localName)
 )
 
-func main() {
+func init() {
+	machine.LED.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	machine.I2C0.Configure(machine.I2CConfig{})
 
-	// time.Sleep(time.Second)
-	// println("starting")
-
+	tempSensor.Configure()
 	floatSensor1.Configure()
 	floatSensor2.Configure()
+	bthome.Configure()
+}
 
-	configureBLE()
-
-	temperatureSupported := false
-	if err := configureTemperatureSensor(); err != nil {
-		println("could not configure temperature sensor:", err.Error())
-	} else {
-		temperatureSupported = true
-	}
+func main() {
 
 	var lastValue1, lastValue2 bool
 	for lastTemp, lastTempRead := int32(0), time.Unix(0, 0); ; {
@@ -57,13 +44,9 @@ func main() {
 		if value1 != lastValue1 {
 			println("float sensor 1:", value1)
 		}
-		ledPin.Set(value1)
+		machine.LED.Set(value1)
+		bthome.SetBool(BTHomeDataBinarySensor1, value1)
 		lastValue1 = value1
-		if value1 {
-			btHomeServiceData[binarySensorIdx1] = 1
-		} else {
-			btHomeServiceData[binarySensorIdx1] = 0
-		}
 
 		// float sensor 2
 		value2 := floatSensor2.Get()
@@ -71,68 +54,53 @@ func main() {
 			println("float sensor 2:", value2)
 		}
 		// ledPin.Set(value2)
+		bthome.SetBool(BTHomeDataBinarySensor2, value2)
 		lastValue2 = value2
-		if value2 {
-			btHomeServiceData[binarySensorIdx2] = 1
-		} else {
-			btHomeServiceData[binarySensorIdx2] = 0
-		}
 
 		// read temperature sensor value
-		if temperatureSupported && time.Since(lastTempRead) > tempReadInterval {
+		if time.Since(lastTempRead) > tempReadInterval {
 			lastTempRead = time.Now()
-			if milliDegreesC, err := readTemperature(); err != nil {
+			if milliDegreesC, err := tempSensor.ReadTemperature(); err != nil {
 				if lastTemp != 0 {
 					println("error reading temperature", err.Error())
 				}
 				lastTemp = 0
-				btHomeServiceData[tempSensorIdx+0] = 0x0
-				btHomeServiceData[tempSensorIdx+1] = 0x0
+				bthome.SetSignedInt16(BTHomeDataTemperature, 0)
 			} else {
 				if lastTemp != milliDegreesC {
 					println("Degrees C", float32(milliDegreesC)/1000, "\n\n")
 				}
 				lastTemp = milliDegreesC
-				btHomeServiceData[tempSensorIdx+0] = byte((milliDegreesC / 100) >> 0)
-				btHomeServiceData[tempSensorIdx+1] = byte((milliDegreesC / 100) >> 8)
+				bthome.SetSignedInt16(BTHomeDataTemperature, int16(milliDegreesC/100))
 			}
 		}
 
 		// advertise values
-		advertise(interval)
+		bthome.Advertise(interval)
 		time.Sleep(interval)
-		stopAdvertisement()
+		bthome.StopAdvertisement()
+
 	}
 
 }
 
-func configureBLE() {
-	must("enable BLE stack", adapter.Enable())
+type TemperatureSensor struct {
+	accel *lsm6ds3.Device
 }
 
-func advertise(interval time.Duration) {
-	adv := adapter.DefaultAdvertisement()
-	must("config adv", adv.Configure(bluetooth.AdvertisementOptions{
-		LocalName:         localName,
-		AdvertisementType: bluetooth.AdvertisingTypeScanInd,
-		Interval:          btHomeAdvInterval,
-		ServiceData: []bluetooth.ServiceDataElement{
-			{UUID: btHomeServiceUUID, Data: btHomeServiceData},
-		},
-	}))
-	must("start adv", adv.Start())
+func NewTemperatureSensor(i2c drivers.I2C) *TemperatureSensor {
+	return &TemperatureSensor{accel: lsm6ds3.New(i2c)}
 }
 
-func stopAdvertisement() {
-	must("stop adv", bluetooth.DefaultAdapter.DefaultAdvertisement().Stop())
+func (t *TemperatureSensor) Configure() error {
+	err := t.accel.Configure(lsm6ds3.Configuration{})
+	return err
 }
 
-func must(action string, err error) {
-	if err != nil {
-		panic("failed to " + action + ": " + err.Error())
-	} else {
-		// println(time.Now().String(), action+" successful")
-	}
+// ReadTemperature returns the temperature in celsius milli degrees (°C/1000)
+func (t *TemperatureSensor) ReadTemperature() (temp int32, err error) {
+	x, err := t.accel.ReadTemperature()
+	return x, err
 }
 
 type FloatSensor struct {
@@ -150,4 +118,65 @@ func (s *FloatSensor) Configure() {
 
 func (s *FloatSensor) Get() bool {
 	return s.gpio.Get()
+}
+
+type BTHome struct {
+	adapter  *bluetooth.Adapter
+	interval bluetooth.Duration
+	svcuuid  bluetooth.UUID
+	svcdata  []byte
+	localnm  string
+}
+
+type BTHomeData int
+
+const (
+	BTHomeDataBinarySensor1 BTHomeData = 2
+	BTHomeDataBinarySensor2 BTHomeData = 4
+	BTHomeDataTemperature   BTHomeData = 6
+)
+
+func NewBTHome(interval time.Duration, localName string) *BTHome {
+	return &BTHome{
+		adapter:  bluetooth.DefaultAdapter,
+		interval: bluetooth.Duration(interval),
+		svcuuid:  bluetooth.New16BitUUID(0xFCD2),
+		svcdata:  []byte{0x40, 0x20, 0x01, 0x20, 0x01, 0x45, 0x00, 0x00},
+		localnm:  localName,
+	}
+}
+
+func (bt *BTHome) SetBool(index BTHomeData, val bool) {
+	if val {
+		bt.svcdata[index] = 1
+	} else {
+		bt.svcdata[index] = 1
+	}
+}
+
+func (bt *BTHome) SetSignedInt16(index BTHomeData, val int16) {
+	bt.svcdata[index+0] = byte(val >> 0)
+	bt.svcdata[index+1] = byte(val >> 8)
+}
+
+func (bt *BTHome) Configure() error {
+	return bt.adapter.Enable()
+}
+
+func (bt *BTHome) Advertise(interval time.Duration) error {
+	adv := bt.adapter.DefaultAdvertisement()
+	opts := bluetooth.AdvertisementOptions{
+		AdvertisementType: bluetooth.AdvertisingTypeScanInd,
+		Interval:          bt.interval,
+		LocalName:         bt.localnm,
+		ServiceData:       []bluetooth.ServiceDataElement{{UUID: bt.svcuuid, Data: bt.svcdata}},
+	}
+	if err := adv.Configure(opts); err != nil {
+		return err
+	}
+	return adv.Start()
+}
+
+func (bt *BTHome) StopAdvertisement() error {
+	return bluetooth.DefaultAdapter.DefaultAdvertisement().Stop()
 }
